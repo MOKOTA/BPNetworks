a381d084507b70022338a4c7858a5f106cc8a4e8
MOKOTA <script_line@outlook.com>
2016-02-21 17:24:14 +0800

    update function randperm

--- pretrement.cpp
+++ pretrement.cpp
@@ -1,17 +1,38 @@
-#include<opencv2/opencv.hpp>
 #include<iostream>
+#include<stdlib.h>
+#include<eigen3/Eigen/Core>
 using namespace std;
-using namespace cv;
+using namespace Eigen;
 
-static void struction()
+VectorXf randperm(const int &Index)
 {
-	std::cout<<"This is a programm of BPnetwork's pretrement "<<std::endl;
-	std::cout<<"pls enter the training picture's location to continue.\n";
 
+    VectorXf result = VectorXf::Random(Index);
+    VectorXf index(Index);
+    for(int k=1;k<=Index;k++)
+        index[k-1]=k;
+    for(int j=0;j<Index-1;j++)
+    {
+        for(int i=0;i<Index-1;i++)
+        {
+            if(result[i+1]>result[i])
+            {
+                float temp;
+                temp = result[i+1];
+                int tempIndex;
+                tempIndex = index[i+1];
+                result[i+1] = result[i];
+                index[i+1] = index[i];
+                result[i] = temp;
+                index[i] = tempIndex;
+            }
+        }
+    }
+    return index;
 }
-/*---------------------全局变量-----------------------*/
-IplImage *origin(NULL);
-IplImage *gray(NULL);
-int main(int argc,char** argv)
+int main()
 {
+    VectorXf v =randperm(10);
+    cout<<v<<endl;
+    return 0;
 }
