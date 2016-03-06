a3ab1b051610056a38b39e8776c9724e97ca56a8
MOKOTA <script_line@outlook.com>
2016-03-06 19:53:51 +0800

    add transform origin image to binary image function to pretrement.cpp

--- pretrement.cpp
+++ pretrement.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 #include<stdlib.h>
+#include<iterator>
+#include<opencv2/opencv.hpp>
 #include<eigen3/Eigen/Core>
+#include<opencv2/highgui/highgui.hpp>
+#include<opencv2/imgproc/imgproc.hpp>
+using namespace cv;
 using namespace std;
 using namespace Eigen;
 
-VectorXf randperm(const int &Index)
+const char *pstrWindowsBinaryTitle = "二值图";
+template<typename T1,typename T2> T1 randperm(const int &Index)
 {
 
-    VectorXf result = VectorXf::Random(Index);
-    VectorXf index(Index);
+    T1 result = T1::Random(Index);
+    T1 index(Index);
     for(int k=1;k<=Index;k++)
         index[k-1]=k;
     for(int j=0;j<Index-1;j++)
@@ -17,7 +23,7 @@ VectorXf randperm(const int &Index)
         {
             if(result[i+1]>result[i])
             {
-                float temp;
+                T2 temp;
                 temp = result[i+1];
                 int tempIndex;
                 tempIndex = index[i+1];
@@ -30,9 +36,46 @@ VectorXf randperm(const int &Index)
     }
     return index;
 }
-int main()
+
+IplImage *TransToBinary(IplImage* img,int pos)
 {
-    VectorXf v =randperm(10);
-    cout<<v<<endl;
+    IplImage *res=NULL;
+    IplImage *gray=NULL;
+    if(!img)
+        return res;
+    int depth = img->depth;
+    int channel = 1;
+    CvSize sz;
+    sz.height = img->height;
+    sz.width = img->width;
+    gray = cvCreateImage(sz,depth,channel);
+    res = cvCreateImage(sz,depth,channel);
+    cvCvtColor(img,gray,CV_BGR2GRAY);
+    cvThreshold(gray,res,100,255,CV_THRESH_BINARY);
+    cvReleaseImage(&gray);
+    return res;
+}
+
+int main(int argc,char** argv)
+{
+    double t = 0;
+    t = (double)cvGetTickCount();
+    char* filename = argv[1];
+    IplImage *image;
+    const char *pstrWindowsToolBarName = "二值图阈值";
+    IplImage *binary;
+
+    image = cvLoadImage(filename);
+
+
+    cvNamedWindow(pstrWindowsBinaryTitle, 2);
+    binary = TransToBinary(image,120);
+    t = (double)cvGetTickCount()-t;
+    cout<<"cost "<<t<<"ms\n";
+    cvShowImage(pstrWindowsBinaryTitle,binary);
+    cvWaitKey(0);
+    cvDestroyAllWindows();
+    cvReleaseImage(&binary);
+    cvReleaseImage(&image);
     return 0;
 }
